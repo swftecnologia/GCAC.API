ae1c3a2d3cb1ad3f082b653b0a2b749d282ad54a
Silvio Silva <silvio.silva@swftecnologia.com.br>
2021-08-13 17:40:57 -0300

    Ajuste para permitir versionar pasta Log do projeto GCAC.Core.

---
+++ GCAC.Core/Entidades/Log/Auditoria.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace GCAC.Core.Entidades.Log
+{
+    /// <summary>
+    /// Entidade para representar log de operações realizadas no banco de dados
+    /// </summary>
+    public record Auditoria
+    {
+        /// <summary>
+        /// Construtor
+        /// </summary>
+        public Auditoria()
+        {
+        }
+
+        /// <summary>
+        /// Identificação da auditoria (PK)
+        /// </summary>
+        public long Id { get; init; }
+
+        /// <summary>
+        /// Dados da auditoria
+        /// </summary>
+        public string Dados { get; set; }
+
+        /// <summary>
+        /// Usuário solicitante
+        /// </summary>
+        public string Usuario { get; set; }
+
+        /// <summary>
+        /// Data da solicitação
+        /// </summary>
+        public DateTime DataOperacao { get; set; }
+    }
+}
