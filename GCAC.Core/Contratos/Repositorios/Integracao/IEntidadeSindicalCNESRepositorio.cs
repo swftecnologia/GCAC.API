3ca0a57b482b20a7bff7469a5d0035c4069023af
Silvio Silva <silvio.silva@swftecnologia.com.br>
2021-10-03 08:50:55 -0300

    Ajustes gerais.

---
+++ GCAC.Core/Contratos/Repositorios/Integracao/IEntidadeSindicalCNESRepositorio.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using GCAC.Core.Entidades.Integracao;
+
+namespace GCAC.Core.Interfaces.Repositorios.Integracao
+{
+    /// <summary>
+    /// Interface de repositório para uma entidade sindical do CNES
+    /// </summary>
+    public interface IEntidadeSindicalCNESRepositorio
+    {
+        /// <summary>
+        /// Cria uma nova entidade sindical do CNES e/ou atualiza uma entidade sindical do CNES existente
+        /// </summary>
+        /// <param name="itens">Lista de novas entidades sindicais do CNES a serem criadas e/ou atualizadas</param>
+        /// <returns>Quantidade de registros afetados pela operação solicitada</returns>
+        Task<int> InserirOuAtualizar(List<EntidadeSindicalListaBaseTerritorialCNES> itens);
+
+        /// <summary>
+        /// Cria uma nova entidade sindical do CNES e/ou atualiza uma entidade sindical do CNES existente
+        /// </summary>
+        /// <param name="itens">Lista de novas entidades sindicais do CNES a serem criadas e/ou atualizadas</param>
+        /// <returns>Quantidade de registros afetados pela operação solicitada</returns>
+        Task<int> InserirOuAtualizar(List<EntidadeSindicalListaCategoriaCNES> itens);
+
+        /// <summary>
+        /// Cria uma nova entidade sindical do CNES e/ou atualiza uma entidade sindical do CNES existente
+        /// </summary>
+        /// <param name="itens">Lista de novas entidades sindicais do CNES a serem criadas e/ou atualizadas</param>
+        /// <returns>Quantidade de registros afetados pela operação solicitada</returns>
+        Task<int> InserirOuAtualizar(List<EntidadeSindicalListaGeralCNES> itens);
+    }
+}
